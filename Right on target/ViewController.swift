7946bd84f19317614c317c04934c5e6bf1c2527b
Vlad <lokotuin12@mail.ru>
2025-08-12 17:48:34 +0300

    Removed the extra click to start

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -13,50 +13,49 @@ class ViewController: UIViewController {
     @IBOutlet var label: UILabel!
     
     var number: Int = 0
-    var round: Int = 0
+    var round: Int = 1
     var points: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("View did load")
+        number = Int.random(in: 1...50)
+        label.text = String(self.number)
     }
     
     @IBAction func checkNumber() {
-        if round == 0 {
-            number = Int.random(in: 1...50)
-            label.text = String(self.number)
-            round = 1
+        let numSlider = Int(slider.value.rounded())
+        
+        if numSlider > number {
+            points += 50 - numSlider + number
+        } else if numSlider < number {
+            points += 50 - number + numSlider
         } else {
-            let numSlider = Int(slider.value.rounded())
-            
-            if numSlider > number {
-                points += 50 - numSlider + number
-            } else if numSlider < number {
-                points += 50 - number + numSlider
-            } else {
-                points += 50
-            }
-            
-            if round == 5 {
-                let alert = UIAlertController(
-                    title: "Игра окончена",
-                    message: "Вы заработали \(points) очков",
-                    preferredStyle: .alert
-                )
-                alert.addAction(UIAlertAction(
-                    title: "Начать заново",
-                    style:.default,
-                    handler: nil)
-                )
-                present(alert, animated: true, completion: nil)
-                
-                round = 1
-                points = 0
-                
-            } else {
-                round += 1
-            }
-            number = Int.random(in: 1...50)
-            label.text = String(number)
+            points += 50
         }
+        
+        if round == 5 {
+            let alert = UIAlertController(
+                title: "Игра окончена",
+                message: "Вы заработали \(points) очков",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(
+                title: "Начать заново",
+                style:.default,
+                handler: nil)
+            )
+            present(alert, animated: true, completion: nil)
+            
+            round = 1
+            points = 0
+            
+        } else {
+            round += 1
+        }
+        number = Int.random(in: 1...50)
+        label.text = String(number)
     }
 }
+
