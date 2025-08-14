2a4d3d10f156e5cf4ddbd69d67e71ec1a96272aa
Vlad <lokotuin12@mail.ru>
2025-08-14 11:05:39 +0300

    FIrst refactoring has been completed

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -8,87 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var game: Game!
     
     @IBOutlet var slider: UISlider!
     @IBOutlet var label: UILabel!
     
-    var number: Int = 0
-    var round: Int = 1
-    var points: Int = 0
-    
-    lazy var secondViewController: SecondViewController = getSecondViewController()
-    
-    override func loadView() {
-        super.loadView()
-        print("loadView")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("ViewDidLoad")
-        number = Int.random(in: 1...50)
-        label.text = String(self.number)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("ViewWillAppear")
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("ViewDidAppear")
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print("ViewWillDisappear")
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print("ViewDidDisappear")
-    }
-    
-    @IBAction func checkNumber() {
-        let numSlider = Int(slider.value.rounded())
-        
-        if numSlider > number {
-            points += 50 - numSlider + number
-        } else if numSlider < number {
-            points += 50 - number + numSlider
-        } else {
-            points += 50
-        }
-        
-        if round == 5 {
-            let alert = UIAlertController(
-                title: "Игра окончена",
-                message: "Вы заработали \(points) очков",
-                preferredStyle: .alert
-            )
-            alert.addAction(UIAlertAction(
-                title: "Начать заново",
-                style:.default,
-                handler: nil)
-            )
-            present(alert, animated: true, completion: nil)
-            
-            round = 1
-            points = 0
-            
-        } else {
-            round += 1
-        }
-        number = Int.random(in: 1...50)
-        label.text = String(number)
+        game = Game(startValue: 1, endValue: 50, rounds: 5)
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
     }
 
-    private func getSecondViewController() -> SecondViewController {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let viewController = storyboard.instantiateViewController(identifier: "SecondViewController")
-        return viewController as! SecondViewController
+    @IBAction func checkNumber() {
+        game.calculateScore(with: Int(slider.value))
+        
+        if game.isGameEnded {
+            showAlertWith(score: game.score)
+            game.restartGame()
+        } else {
+            game.startRound()
+        }
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
+    }
+
+    private func updateLabelWithSecretNumber(newText: String ) {
+        label.text = newText
+    }
+    
+    private func showAlertWith(score: Int) {
+        let alert = UIAlertController(
+            title: "Game Over",
+            message: "Your score is \(score)",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(
+            title: "Start New Game",
+            style: .default,
+            handler: nil)
+        )
+        present(alert, animated: true)
     }
 }
 
